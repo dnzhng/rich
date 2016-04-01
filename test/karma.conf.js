06ce1dec3b44efb636d34f4b4df38f88c6297969
Dan Zhang <danzhang@Dans-MacBook-Pro-2.local>
2016-04-01 11:38:23 -0700

    Test if travis will only run phantom or try to run chrome as well

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -2,7 +2,7 @@ var webpack = require('webpack');
 
 module.exports = function (config) {
   config.set({
-    browsers: [ 'PhantomJS' ], //run in Chrome
+    browsers: [ 'PhantomJS', process.env.CONTINUOUS_INTEGRATION ? '' : 'Chrome' ], //run in only phantom on CI
     singleRun: true, //just run once by default
     frameworks: [ 'mocha' ], //use the mocha test framework
     colors: true,
