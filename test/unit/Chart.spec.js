bea719199198951079935bf1523728dc26343934
Dan Zhang <danzhang@Dans-MacBook-Pro-2.local>
2016-04-26 13:21:38 -0700

    [#4] Adding more granular tests to chart.spec.js

--- test/unit/Chart.spec.js
+++ test/unit/Chart.spec.js
@@ -19,6 +19,14 @@ describe('<Chart />', () => {
     expect(chart.find(".rich-chart")).to.have.length(1);
   });
 
+  it('Should change class name depending on type passed in', () => {
+    expect(chart.find(".LineGraph")).to.have.length(1);
+    chart = shallow(<Chart type="PieChart" data={"data"} />);
+    expect(chart.find(".LineGraph")).to.have.length(0);
+    expect(chart.find(".PieChart")).to.have.length(1);
+
+  });
+
   it('Should have correct props', () => {
     expect(chart.props.type).to.be.truthy;
     expect(chart.props.data).to.be.truthy;
