ffc3feddeb365ff134edf418539d94df9dd2950e
Dan Zhang <danzhang@Dans-MacBook-Pro-2.local>
2016-04-27 12:56:28 -0700

    [#7] Build basic richbanner and add to docs site

--- docs/src/js/home.js
+++ docs/src/js/home.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router';
-
+import { RichBanner } from '../../../src'
 class Home extends React.Component {
 
   render() {
     return (
-      <div>
-        <h1>Rich React Components</h1>
+      <div className="rich-home">
+        <RichBanner className="brand" height={50} >
+          <h1 className="brand-logo">Rich</h1>
+          <h3 className="brand-description">A React component library to help you build your applications</h3>
+        </RichBanner>
         <Link to={`/docs`}>Docs</Link>
       </div>
     )
---
+++ src/RichBanner/RichBanner.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class RichBanner extends React.Component {
+  static propTypes = {
+    height: React.PropTypes.number.isRequired
+  }
+
+  render() {
+    let style = {
+      height : this.props.height + "vh"
+    }
+    return (
+      <div className={"rich-banner"} style={style}>
+        <div className={this.props.className}>
+          {this.props.children}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default RichBanner
