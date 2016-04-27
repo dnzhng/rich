2b21fbac6e7671af725782d05e0e748fee0c84c1
Dan Zhang <danzhang@Dans-MacBook-Pro-2.local>
2016-04-27 13:00:05 -0700

    [#7] add richbanner tests

---
+++ test/unit/RichBanner.spec.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import { expect } from 'chai';
+import RichBanner from '../../src/RichBanner/';
+
+describe('<RichBanner />', () => {
+  let banner;
+
+
+  beforeEach(() => {
+    banner = shallow(<RichBanner height={50} />);
+  })
+
+  it('Should mount a div within a div to the dom', () => {
+    expect(banner.find('div')).to.have.length(2);
+  });
+
+  it('Should have classname rich-banner', () => {
+    expect(banner.find(".rich-banner")).to.have.length(1);
+  });
+
+  it('Should have correct props', () => {
+    expect(banner.props.height).to.be.truthy;
+  });
+
+});
